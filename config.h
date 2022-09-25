5e2b6d24b03a81468c77b4c700169134e1b8ea7d
Nischal Shrestha <nischalstha9@gmail.com>
2022-09-25 07:54:05 +0545

    added border color and padding

--- config.h
+++ config.h
@@ -12,7 +12,8 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+//top bar second color (blue) and active window border color
+static const char col_cyan[]        = "#ffae00";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
